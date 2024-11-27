2ee67f74c1ce1bf80ab3428fc402e081b0c57ceb
Quang <qth9368@rit.edu>
2024-11-27 13:02:50 -0500

    incorporated components into App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Experience from './components/Experience';
+import Education from './components/Education';
+import Projects from './components/Projects';
+import TechStack from './components/TechStack';
+import Socials from './components/Socials';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <Header />
+      <div className="content">
+        <Experience />
+        <Education />
+        <Projects />
+        <TechStack />
+        <Socials />
+      </div>
+      <Footer />
     </div>
   );
 }
