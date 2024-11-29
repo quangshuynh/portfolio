949b2389d38fd82dbf7cd7d4ea5d83c34836d0e3
Quang <qth9368@rit.edu>
2024-11-29 17:37:41 -0500

    implemented tech stack in projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -16,42 +16,49 @@ function Projects() {
       about: 'A Flask-based recipe generation app that uses Ollama LLM and Weaviate DB to create unique meal ideas',
       logo: ai_cook, 
       github: 'https://github.com/quangshuynh/AI-Cooking-Assistant/', 
+      techStack: ['Python', 'Flask', 'Ollama LLM', 'Weaviate DB', 'HTML', 'CSS', 'JavaScript'],
     },
     {
       name: 'Personalized Budget Assistant',
       about: 'A Flask-based web app to track expenses, categorize transactions and visualize spending with dark/light mode support',
       logo: budget_assistant, 
       github: 'https://github.com/quangshuynh/Personalized-Budgeting-Assistant', 
+      techStack: ['Python', 'Flask', 'JavaScript', 'HTML', 'CSS'],
     },
     {
       name: 'AI Panel Game App',
       about: 'A Tkinkter-based GUI app that simulates an interactive panel of AI agents with unique personalities, responding to user questions and engaging in multi-agent conversations using Ollama LLM',
       logo: ai_panel, 
-      github: 'https://github.com/quangshuynh/AI-Panelist-GUI', 
+      github: 'https://github.com/quangshuynh/AI-Panelist-GUI',
+      techStack: ['Python', 'Tkinter', 'LLM'], 
     },
     {
       name: 'Password Manager',
       about: 'Secure, GUI-based password manager using cryptography, and customtkinter. Built collaboratively with a focus on usability and encryption',
       logo: pw_manager, 
       github: 'https://github.com/quangshuynh/passwordmanager', 
+      techStack: ['Python', 'CustomTkinter', 'Fernet'],
     },
     {
       name: 'Car Price Prediction',
       about: 'A machine learning model built with PyTorch to predict the price of used cars based on key features',
       logo: car_price_prediction, 
       github: 'https://github.com/quangshuynh/car-prices-prediction/', 
+      techStack: ['Python', 'PyTorch', 'NumPy', 'Pandas', 'HTML', 'CSS', 'JavaScript'],
     },
     {
       name: 'Astro & Hoppers Game',
       about: 'A 2D puzzle game inspired by Lunar Landing, built with JavaFX, PTUI, MVC design pattern and BFS algorithms',
       logo: astro_game, 
       github: 'https://github.com/quangshuynh/javafx-board-game/', 
+      techStack: ['Java', 'JavaFX', 'BFS Algorithm'],
     },
     {
       name: 'Ngram Text Statistics',
       about: 'Analyzed 4GB of Google Books Ngram data to visualize trends, compute word similarities and uncover linguistic insights using Matplotlib & NumPy',
       logo: ngram, 
       github: 'https://github.com/quangshuynh/ngram/', 
+      techStack: ['Python', 'NumPy', 'Matplotlib'],
     },
   ];
 
@@ -62,17 +69,22 @@ function Projects() {
         {projects.map((project, index) => (
           <div key={index} className="project-card">
             <img src={project.logo} alt={`${project.name} logo`} className="project-logo" />
+            <a
+              href={project.github}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="github-link"
+            >
+              <FaGithub className="github-icon" /> GitHub Repo
+            </a>
             <div className="project-details">
               <h3>{project.name}</h3>
               <p>{project.about}</p>
-              <a
-                href={project.github}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="github-link"
-              >
-                <FaGithub className="github-icon" /> GitHub Repo
-              </a>
+              <ul className="tech-stack">
+                {project.techStack.map((tech, index) => (
+                  <li key={index} className="tech-item">{tech}</li>
+                ))}
+              </ul>
             </div>
           </div>
         ))}
