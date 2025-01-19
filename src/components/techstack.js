cc3d3c1253024f16cb3a5d41e8bda8d6295e68cb
Quang <qth9368@rit.edu>
2025-01-19 15:49:30 -0500

    updated tech stak

--- src/components/techstack.js
+++ src/components/techstack.js
@@ -2,34 +2,38 @@ import React from 'react';
 import '../styles/techstack.css';
 
 function TechStack() {
+  // https://svglogos.dev/ (for logos)
   const languages = [
-    { name: 'Python', logo: 'https://cdn.svgporn.com/logos/python.svg', proficiency: 'Expert' },
-    { name: 'HTML', logo: 'https://cdn.svgporn.com/logos/html-5.svg', proficiency: 'Expert' },
-    { name: 'CSS', logo: 'https://cdn.svgporn.com/logos/css-3.svg', proficiency: 'Expert' },
-    { name: 'Java', logo: 'https://cdn.svgporn.com/logos/java.svg', proficiency: 'Advanced' },
-    { name: 'JavaScript', logo: 'https://cdn.svgporn.com/logos/javascript.svg', proficiency: 'Advanced' },
-    { name: 'C', logo: 'https://cdn.svgporn.com/logos/c.svg', proficiency: 'Intermediate' },
-    { name: 'C#', logo: 'https://cdn.svgporn.com/logos/c-sharp.svg', proficiency: 'Beginner' },
+    { name: 'Python', logo: 'https://cdn.svgporn.com/logos/python.svg'},
+    { name: 'HTML', logo: 'https://cdn.svgporn.com/logos/html-5.svg'},
+    { name: 'CSS', logo: 'https://cdn.svgporn.com/logos/css-3.svg'},
+    { name: 'Java', logo: 'https://cdn.svgporn.com/logos/java.svg'},
+    { name: 'JavaScript', logo: 'https://cdn.svgporn.com/logos/javascript.svg'},
+    { name: 'C', logo: 'https://cdn.svgporn.com/logos/c.svg'},
+    { name: 'C#', logo: 'https://cdn.svgporn.com/logos/c-sharp.svg'},
+    { name: 'SQL', logo: 'https://i.imgur.com/0cgPCGd.png'},
   ];
   const frameworks = [
-    { name: 'Flask', logo: 'https://cdn.svgporn.com/logos/flask.svg', proficiency: 'Expert' },
-    { name: 'React', logo: 'https://cdn.svgporn.com/logos/react.svg', proficiency: 'Advanced' },
-    { name: 'JavaFX', logo: 'https://cdn.svgporn.com/logos/java.svg', proficiency: 'Intermediate' },
-    { name: 'Tkinter', logo: 'https://i.imgur.com/g6am5XO.png', proficiency: 'Intermediate' },
-    { name: 'PyTorch', logo: 'https://cdn.svgporn.com/logos/pytorch-icon.svg', proficiency: 'Intermediate' },
-    { name: 'LLMs', logo: 'https://registry.npmmirror.com/@lobehub/icons-static-png/latest/files/dark/ollama.png', proficiency: 'Intermediate' },
-    { name: 'Weaviate', logo: 'https://d11a6trkgmumsb.cloudfront.net/original/4X/8/1/1/811ef156e2525559c859ecdb7a5cd26d5e459e46.png', proficiency: 'Intermediate' },
-    { name: 'NumPy', logo: 'https://cdn.svgporn.com/logos/numpy.svg', proficiency: 'Advanced' },
-    { name: 'Pandas', logo: 'https://cdn.svgporn.com/logos/pandas-icon.svg', proficiency: 'Advanced' },
-    { name: 'Matplotlib', logo: 'https://cdn.svgporn.com/logos/matplotlib.svg', proficiency: 'Advanced' },
-    { name: 'Computer Vision', logo: 'https://cdn.svgporn.com/logos/opencv.svg', proficiency: 'Beginner' },
+    { name: 'Flask', logo: 'https://cdn.svgporn.com/logos/flask.svg'},
+    { name: 'React', logo: 'https://cdn.svgporn.com/logos/react.svg'},
+    { name: 'JavaFX', logo: 'https://cdn.svgporn.com/logos/java.svg'},
+    { name: 'Tkinter', logo: 'https://i.imgur.com/g6am5XO.png'},
+    { name: 'PyTorch', logo: 'https://cdn.svgporn.com/logos/pytorch-icon.svg'},
+    { name: 'LLMs', logo: 'https://registry.npmmirror.com/@lobehub/icons-static-png/latest/files/dark/ollama.png'},
+    { name: 'Weaviate', logo: 'https://d11a6trkgmumsb.cloudfront.net/original/4X/8/1/1/811ef156e2525559c859ecdb7a5cd26d5e459e46.png'},
+    { name: 'NumPy', logo: 'https://cdn.svgporn.com/logos/numpy.svg'},
+    { name: 'Pandas', logo: 'https://cdn.svgporn.com/logos/pandas-icon.svg'},
+    { name: 'Matplotlib', logo: 'https://cdn.svgporn.com/logos/matplotlib.svg'},
+    { name: 'Computer Vision', logo: 'https://cdn.svgporn.com/logos/opencv.svg'},
   ];
   const tools = [
     { name: 'Git', logo: 'https://cdn.svgporn.com/logos/git-icon.svg' },
     { name: 'Linux', logo: 'https://cdn.svgporn.com/logos/linux-tux.svg' },
+    { name: 'Visual Studio Code', logo: 'https://cdn.svgporn.com/logos/visual-studio-code.svg' },
+    { name: 'Visual Studio', logo: 'https://cdn.svgporn.com/logos/visual-studio.svg' },
     { name: 'PyCharm', logo: 'https://cdn.svgporn.com/logos/pycharm.svg' },
     { name: 'IntelliJ', logo: 'https://cdn.svgporn.com/logos/intellij-idea.svg' },
-    { name: 'VSCode', logo: 'https://cdn.svgporn.com/logos/visual-studio-code.svg' },
+    { name: 'SQL Server Management Studio', logo: 'https://i.imgur.com/lWJDvnV.png' },
     { name: 'Replit', logo: 'https://cdn.svgporn.com/logos/replit-icon.svg' },
     { name: 'MobaXterm', logo: 'https://onion.io/wp-content/uploads/2017/11/Moba-Logo.jpg' },
   ];
