08882ba9c0fe7329bd55a8b15ac3c68ddb399520
Quang <qth9368@rit.edu>
2025-05-25 21:07:53 -0400

    kore summer note

--- src/components/experience.js
+++ src/components/experience.js
@@ -36,7 +36,7 @@ function Experience() {
     {
       company: 'People Inc.',
       role: 'Self Determination Assistant',
-      timeline: 'Jul. 2024 - Present',
+      timeline: 'Jul. 2024 - Apr. 2025',
       url: 'https://www.people-inc.org/about/people-inc/',
       logo: people_inc_logo,
       about: [
