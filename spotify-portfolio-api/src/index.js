1d0c3bcb36f31fb662165ba48f634a5f44e372d5
Quang <20378quang@gmail.com>
2026-09-07 09:19:56 -0400

    fix: allowed vercel and quanghuynh.com for spotify cors

--- spotify-portfolio-api/src/index.js
+++ spotify-portfolio-api/src/index.js
@@ -16,6 +16,9 @@ const SPOTIFY_RECENT_URL =
 
 const ALLOWED_ORIGINS = new Set([
   'https://quangshuynh.github.io',
+  'https://quangs.vercel.app',
+  'https://quanghuynh.com',
+  'https://www.quanghuynh.com',
   'http://localhost:3000',
   'http://127.0.0.1:3000',
   'http://localhost:5173',
